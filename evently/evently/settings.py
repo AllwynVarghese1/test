d360571ff85fbb6865867ef84ce226e8e49f5d2a
AllwynVarghese1 <77040743+AllwynVarghese1@users.noreply.github.com>
2021-12-17 12:32:22 +0530

    Update settings.py

--- evently/evently/settings.py
+++ evently/evently/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-onqv(!-#5!q7%2ik!m4c$hjq)=fx(ph)bux6m6v#6^45=r27@8'
+SECRET_KEY = 'test'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
